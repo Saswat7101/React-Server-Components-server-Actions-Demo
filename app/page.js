2f00f5c03e547abc9acbe54938d73ec38dc3dce7
Saswat <mohantysaswat154@gmail.com>
2026-02-14 17:17:17 +0530

    fetching data with RSCs

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
-import ClientDemo from "@/components/ClientDemo";
-import RSCDemo from "@/components/RSCDemo";
+// import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
+// import RSCDemo from "@/components/RSCDemo";
 
 export default function Home() {
   return (
     <main>
-      <ClientDemo>
-        <RSCDemo />
-      </ClientDemo>
+      <DataFetchingDemo />
     </main>
   );
 }
