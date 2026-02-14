f3624a888b2d41a43cef2a77777685c8ca1ce5fd
Saswat <mohantysaswat154@gmail.com>
2026-02-14 17:38:30 +0530

    submitting data with server actions

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 // import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
+import ServerActionsDemo from "@/components/ServerActionsDemo";
 // import RSCDemo from "@/components/RSCDemo";
 
 export default function Home() {
   return (
     <main>
+      <ServerActionsDemo />
       <DataFetchingDemo />
     </main>
   );
